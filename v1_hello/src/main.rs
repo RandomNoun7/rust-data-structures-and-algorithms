c682621137b14b703d470a6121acc1cb63cec55e
Bill Hurt <hurt.bill@gmail.com>
2024-12-09 21:40:33 -0800

    Color Enum

--- v1_hello/src/main.rs
+++ v1_hello/src/main.rs
@@ -1,7 +1,10 @@
+#[derive(Debug)]
+#[allow(dead_code)]
 pub struct Person {
     name: String,
     age: i32,
     children: i32,
+    fave_color: Color,
 }
 
 impl Person {
@@ -12,12 +15,29 @@ impl Person {
         )
     }
 }
+
+#[derive(Debug)]
+pub enum Color {
+    Red(String),
+    Green,
+    Blue,
+}
+
 fn main() {
     let p = Person {
         name: "matt".to_string(),
         age: 35,
         children: 4,
+        fave_color: Color::Green,
     };
 
-    println!("Hello, people, from {}", p.print());
+    let c = Color::Red("hello".to_string());
+
+    match c {
+        Color::Red(s) => println!("It's red! {}", s),
+        Color::Green => println!("It's green!"),
+        Color::Blue => println!("It's blue!"),
+    }
+
+    println!("Hello, people, from {:?}", p);
 }
