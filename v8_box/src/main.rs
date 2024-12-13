d638f7f09debcfe9976fc53aa9fb2880e7617fd0
Bill Hurt <hurt.bill@gmail.com>
2024-12-13 15:44:34 -0800

    lifetimes

--- v8_box/src/main.rs
+++ v8_box/src/main.rs
@@ -33,4 +33,34 @@ fn main() {
 
     println!("v.len = {}, capacity = {}", v.len(), v.capacity());
     println!("Hello, {:?}", ll);
+
+    let mut s = "  hello  ".to_string();
+    let p = s.trim();
+    let p = p.to_string();
+
+    s.push_str("goodbye");
+
+    println!("p == '{}'", p);
+
+    let fstr = "help me find home";
+    let ffstr = string_find_f(&fstr);
+    println!("ffstr = {}", ffstr);
+    println!("chosen = {}", choose_str(1));
+}
+
+fn string_find_f(s: &str) -> &str {
+    for (n, x) in s.char_indices() {
+        if x == 'f' {
+            return &s[n..];
+        }
+    }
+    s
+}
+
+fn choose_str(n: i32) -> &'static str {
+    match n {
+        0 => "hello",
+        1 => "goodbye",
+        _ => "other",
+    }
 }
