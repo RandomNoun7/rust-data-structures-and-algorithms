0468c6216cf0cafe7766e86638cb47de6f86b745
Bill Hurt <hurt.bill@gmail.com>
2024-12-13 22:15:55 -0800

    Merge Sort

--- v1-sorting/src/lib.rs
+++ v1-sorting/src/lib.rs
@@ -5,14 +5,59 @@ pub fn bubble_sort<T: PartialOrd + Debug>(v: &mut [T]) {
         let mut sorted = true;
         for i in 0..(v.len() - 1) - p {
             if v[i] > v[i + 1] {
-                println!("{:?}", v);
-                println!("{:?} is greater than {:?}", v[i], v[i + 1]);
                 v.swap(i, i + 1);
                 sorted = false;
             }
         }
         if sorted {
             return;
+        }
+    }
+}
+
+pub fn merge_sort<T: PartialOrd + Debug>(mut v: Vec<T>) -> Vec<T> {
+    // Sort the left half,
+    // Sort the right half, O(n * log(n))
+    // Bring the sorted halfs together O(n)
+
+    if v.len() <= 1 {
+        return v;
+    }
+
+    let mut res = Vec::with_capacity(v.len());
+    let b = v.split_off(v.len() / 2);
+    let a = merge_sort(v);
+    let b = merge_sort(b);
+
+    let mut a_it = a.into_iter();
+    let mut b_it = b.into_iter();
+    let mut a_peek = a_it.next();
+    let mut b_peek = b_it.next();
+
+    loop {
+        match a_peek {
+            Some(ref a_val) => match b_peek {
+                Some(ref b_val) => {
+                    if b_val < a_val {
+                        res.push(b_peek.take().unwrap());
+                        b_peek = b_it.next();
+                    } else {
+                        res.push(a_peek.take().unwrap());
+                        a_peek = a_it.next();
+                    }
+                }
+                None => {
+                    res.push(a_peek.take().unwrap());
+                    a_peek = a_it.next();
+                }
+            },
+            None => {
+                if let Some(b_val) = b_peek {
+                    res.push(b_val);
+                }
+                res.extend(b_it);
+                return res;
+            }
         }
     }
 }
@@ -25,6 +70,13 @@ mod tests {
     fn test_bubble_sort() {
         let mut v = vec![4, 6, 1, 8, 11, 13, 3];
         bubble_sort(&mut v);
-        assert_eq!(v, vec![3, 1, 4, 6, 8, 11, 13]);
+        assert_eq!(v, vec![1, 3, 4, 6, 8, 11, 13]);
+    }
+
+    #[test]
+    fn test_merge_sort() {
+        let v = vec![4, 6, 1, 8, 11, 13, 3];
+        let v = merge_sort(v);
+        assert_eq!(v, vec![1, 3, 4, 6, 8, 11, 13]);
     }
 }
