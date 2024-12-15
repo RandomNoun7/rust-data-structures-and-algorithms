49cb3fed135d527a5944a8be19138bfecc0464ad
Bill Hurt <hurt.bill@gmail.com>
2024-12-14 20:25:16 -0800

    Quick sort

--- v1-sorting/src/lib.rs
+++ v1-sorting/src/lib.rs
@@ -62,6 +62,35 @@ pub fn merge_sort<T: PartialOrd + Debug>(mut v: Vec<T>) -> Vec<T> {
     }
 }
 
+// Move first element to the correct place
+// Everything lower should be before it,
+// everything higher should be after it
+// return it's lovation
+pub fn pivot<T: PartialOrd>(v: &mut [T]) -> usize {
+    let mut p = 0;
+    for i in 1..v.len() {
+        if v[i] < v[p] {
+            // move our pivot forward 1, and put this element before it
+            v.swap(p + 1, i);
+            v.swap(p, p + 1);
+            p += 1
+        }
+    }
+    p
+}
+
+pub fn quick_sort<T: PartialOrd + Debug>(v: &mut [T]) {
+    if v.len() <= 1 {
+        return;
+    }
+    let p = pivot(v);
+    println!("{:?}", v);
+
+    let (a, b) = v.split_at_mut(p);
+    quick_sort(a);
+    quick_sort(&mut b[1..]);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -78,5 +107,27 @@ mod tests {
         let v = vec![4, 6, 1, 8, 11, 13, 3];
         let v = merge_sort(v);
         assert_eq!(v, vec![1, 3, 4, 6, 8, 11, 13]);
+    }
+
+    #[test]
+    fn test_pivot() {
+        let mut v = vec![4, 6, 1, 19, 8, 11, 13, 3];
+        let p = pivot(&mut v);
+
+        for x in 0..v.len() {
+            assert!((v[x] < v[p]) == (x < p))
+        }
+        // assert_eq!(v, vec![1, 3, 4, 6, 19, 8, 11, 13]);
+    }
+
+    #[test]
+    fn test_quick_sort() {
+        let mut v = vec![4, 6, 1, 8, 11, 13, 3];
+        quick_sort(&mut v);
+        assert_eq!(v, vec![1, 3, 4, 6, 8, 11, 13]);
+
+        let mut v = vec![1, 2, 6, 7, 9, 12, 13, 14];
+        quick_sort(&mut v);
+        assert_eq!(v, vec![1, 2, 6, 7, 9, 12, 13, 14]);
     }
 }
