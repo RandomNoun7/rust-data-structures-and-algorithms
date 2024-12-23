0238a02cfd6fcce7740cc94d8ef02546119de742
Bill Hurt <hurt.bill@gmail.com>
2024-12-22 20:47:37 -0800

    Balance tree

--- binary_tree/src/main.rs
+++ binary_tree/src/main.rs
@@ -6,6 +6,7 @@ pub struct BinTree<T>(Option<Box<BinData<T>>>);
 #[derive(Debug)]
 pub struct BinData<T> {
     data: T,
+    h: i8, // Could be boolean for Red and Black, but maths is easier with a small int.
     left: BinTree<T>,
     right: BinTree<T>,
 }
@@ -14,26 +15,67 @@ impl<T> BinTree<T> {
     pub fn new() -> Self {
         BinTree(None)
     }
+
+    pub fn height(&self) -> i8 {
+        match self.0 {
+            Some(ref t) => t.h,
+            None => 0,
+        }
+    }
+
+    pub fn set_height(&mut self) {
+        if let Some(ref mut t) = self.0 {
+            t.h = 1 + std::cmp::max(t.left.height(), t.right.height());
+        }
+    }
+
+    pub fn rot_left(&mut self) {
+        self.0 = self.0.take().map(|v| v.rot_left());
+    }
+
+    pub fn rot_right(&mut self) {
+        self.0 = self.0.take().map(|v| v.rot_right());
+    }
 }
 
 impl<T: PartialOrd> BinTree<T> {
     pub fn add_sorted(&mut self, data: T) {
-        match self.0 {
+        let rot_dir = match self.0 {
             Some(ref mut bd) => {
                 if data < bd.data {
                     bd.left.add_sorted(data);
+                    if bd.left.height() - bd.right.height() > 1 {
+                        1
+                    } else {
+                        0
+                    }
                 } else {
                     bd.right.add_sorted(data);
+                    if bd.right.height() - bd.left.height() > 1 {
+                        -1
+                    } else {
+                        0
+                    }
                 }
             }
             None => {
                 self.0 = Some(Box::new(BinData {
                     data,
+                    h: 0,
                     left: BinTree::new(),
                     right: BinTree::new(),
-                }))
+                }));
+                0
             }
+        };
+
+        match rot_dir {
+            1 => self.rot_right(),
+            -1 => self.rot_left(),
+            _ => self.set_height(),
         }
+
+        self.set_height();
     }
 }
 
@@ -45,21 +87,62 @@ impl<T: Debug> BinTree<T> {
             for _ in 0..dp {
                 spc.push('.');
             }
-            println!("{}{:?}", spc, bd.data);
+            println!("{}:{}{:?}", bd.h, spc, bd.data);
             bd.right.print_lfirst(dp + 1)
         }
+    }
+}
+
+impl<T> BinData<T> {
+    pub fn rot_left(mut self) -> Box<Self> {
+        // result is the right hand node
+        let mut res = match self.right.0.take() {
+            Some(res) => res,
+            None => return Box::new(self),
+        };
+
+        // move left of right node to the right of the start node
+        self.right = BinTree(res.left.0.take());
+        self.right.set_height();
+        // set the results of the left node to the start node
+        res.left = BinTree(Some(Box::new(self)));
+        res.left.set_height();
+        res.h = 1 + std::cmp::max(res.left.height(), res.right.height());
+        res
+    }
+
+    pub fn rot_right(mut self) -> Box<Self> {
+        // result is the left hand node
+        let mut res = match self.left.0.take() {
+            Some(res) => res,
+            None => return Box::new(self),
+        };
+
+        // move left of right node to the right of the start node
+        self.left = BinTree(res.right.0.take());
+        self.left.set_height();
+        // set the results of the left node to the start node
+        res.right = BinTree(Some(Box::new(self)));
+        res.right.set_height();
+        res.h = 1 + std::cmp::max(res.left.height(), res.right.height());
+        res
     }
 }
 
 fn main() {
     let mut t = BinTree::new();
 
-    t.add_sorted(1);
-    t.add_sorted(12);
-    t.add_sorted(6);
-    t.add_sorted(8);
-    t.add_sorted(3);
+    t.add_sorted(4);
     t.add_sorted(5);
+    t.add_sorted(6);
+    t.add_sorted(10);
+    t.add_sorted(1);
+    t.add_sorted(94);
+    t.add_sorted(54);
+    t.add_sorted(3);
 
+    for i in 0..100000 {
+        t.add_sorted(i);
+    }
     t.print_lfirst(0);
 }
