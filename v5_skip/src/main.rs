a06d351392aac9297d1c2ad6122207319cbb29f8
Bill Hurt <hurt.bill@gmail.com>
2025-01-07 20:44:03 -0800

    Skip list part 2

--- v5_skip/src/main.rs
+++ v5_skip/src/main.rs
@@ -1,4 +1,6 @@
 use std::cell::RefCell;
+use std::fmt::{self, write};
+use std::fmt::{Debug, Write};
 use std::rc::Rc;
 
 type Rcc<T> = Rc<RefCell<T>>;
@@ -13,6 +15,8 @@ pub struct SkipNode<T: PartialOrd> {
     down: Option<Rcc<SkipNode<T>>>,
     data: Rcc<T>,
 }
+
+pub struct SkipList<T: PartialOrd>(Vec<SkipNode<T>>);
 
 impl<T: PartialOrd> SkipNode<T> {
     pub fn new(t: T) -> Self {
@@ -61,8 +65,84 @@ impl<T: PartialOrd> SkipNode<T> {
     }
 }
 
+impl<T: Debug + PartialOrd> SkipNode<T> {
+    pub fn print_row<W: Write>(&self, w: &mut W) -> std::fmt::Result {
+        write!(w, "{:?}", self.data.borrow())?;
+        if let Some(ref r) = self.right {
+            write!(w, ",")?;
+            r.borrow().print_row(w)?;
+        }
+        Ok(())
+    }
+}
+
+impl<T: PartialOrd> SkipList<T> {
+    pub fn new() -> Self {
+        SkipList(Vec::new())
+    }
+
+    pub fn insert(&mut self, data: T) {
+        if self.0.len() == 0 {
+            self.0.push(SkipNode::new(data));
+            return;
+        }
+        // Our vec will have the lowest row, with the lowest number,
+        // we need to try and insert in the highest available row.
+        for i in (0..self.0.len()).rev() {
+            if data > *self.0[i].data.borrow() {
+                if let Some(ch) = self.0[i].insert(data) {
+                    self.loop_up(ch, i + 1);
+                }
+                return;
+            }
+        }
+        // if non of those succeeded, that means we have an element to replace the first
+        let mut nn = SkipNode::new(data);
+        std::mem::swap(&mut nn, &mut self.0[0]); // put our new element on the front of the row
+        let res = rcc(nn);
+        self.0[0].right = Some(res.clone());
+        self.loop_up(res, 1);
+    }
+
+    pub fn loop_up(&mut self, ch: Rcc<SkipNode<T>>, n: usize) {
+        if rand::random::<bool>() == true {
+            return;
+        }
+        let dt = ch.borrow().data.clone();
+        let mut nn = SkipNode {
+            right: None,
+            down: Some(ch),
+            data: dt,
+        };
+        if n >= self.0.len() {
+            self.0.push(nn);
+            return;
+        }
+
+        std::mem::swap(&mut nn, &mut self.0[n]);
+        let res = rcc(nn);
+        self.0[n].right = Some(res.clone());
+        self.loop_up(res, n + 1);
+    }
+}
+
+impl<T: Debug + PartialOrd> fmt::Display for SkipList<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if self.0.len() == 0 {
+            return write!(f, "SkipList<Empty>");
+        }
+
+        for sn in &self.0 {
+            write!(f, "\n")?;
+            sn.print_row(f)?;
+        }
+
+        Ok(())
+    }
+}
+
 fn main() {
-    let mut s = SkipNode::new(4);
+    let mut s = SkipList::new();
 
     s.insert(4);
     s.insert(6);
@@ -70,5 +150,5 @@ fn main() {
     s.insert(84);
     s.insert(23);
     s.insert(1);
-    println!("s = {:?}", s);
+    println!("s = {}", s);
 }
